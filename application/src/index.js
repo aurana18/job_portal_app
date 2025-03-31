dad1ca8500a571efeaff952506bb379a4eb6b0fd
AurRan <115421349+aurana18@users.noreply.github.com>
2025-03-31 02:48:16 +0100

    Delete application directory

--- application/src/index.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client'; // Correct import for React 18
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-
-
-
